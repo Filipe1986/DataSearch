4dc58c167b49d525b4b2877fc6ebd7cd66d2fb35
filipe <filipe.esteves.goncalves@gmail.com>
2022-05-19 19:57:15 +0100

    Remove constants from previous project

--- datasearch/src/main/java/com/filipe/datasearch/constants/Constants.java
+++ datasearch/src/main/java/com/filipe/datasearch/constants/Constants.java
@@ -14,19 +14,6 @@ public class Constants {
 		private Url() {}
 		
 		public static final String SEARCH_PATH = "/search";
-		public static final String FIZZBUZZ_ENTRIES = "entry";
-	}
-	
-	public static class ReturnMessage {
-		private ReturnMessage() {}
-		
-		public static final String FIZZ = "fizz";
-		public static final String BUZZ = "buzz";
-		public static final String FIZZBUZZ = "fizzbuzz";
-		public static final String BAZZ = "Bazz";
-		public static final String FIZZBAZZ = "FizzBazz";
-		public static final String BUZZBAZZ = "BuzzBazz";	
-		public static final String FIZZBUZZBAZZ = "FizzBuzzBazz";	
-	}
 
+	}
 }
